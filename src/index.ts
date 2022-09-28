ae57bdbde556dbd54decc3782efdd6d6c0d0d652
Onda Reki <rekionda0524@ezweb.ne.jp>
2022-09-28 22:00:15 +0900

    index.ts一部修正

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 const PORT: string | number = process.env.PORT || 8000;
 const app: express.Application = express();
 
-app.get("/hello", async (req: express.Request, res: express.Response) => {
-  res.send({ message: "hello" });
+app.get('/hello', async (req: express.Request, res: express.Response) => {
+  res.send({ message: 'hello' });
 });
 
 app.listen(PORT, () => {
