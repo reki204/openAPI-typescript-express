3ece84217502a93ac2f5c87ccb217a8bd12999ff
Onda Reki <rekionda0524@ezweb.ne.jp>
2022-09-28 20:01:03 +0900

    server追加

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Application } from "express";
+const PORT = process.env.PORT || 8000;
+const app: Application = express();
+
+app.get("/ping", async (req, res) => {
+  res.send({
+    message: "pong",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
+});
