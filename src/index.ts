e18c029fe6e750d12bf7f59b16a5b6cfda858d82
Onda Reki <rekionda0524@ezweb.ne.jp>
2022-09-28 21:50:49 +0900

    index.ts修正

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
-import express, { Application } from "express";
-const PORT = process.env.PORT || 8000;
-const app: Application = express();
+import express from "express";
+const PORT: string | number = process.env.PORT || 8000;
+const app: express.Application = express();
 
-app.get("/ping", async (req, res) => {
-  res.send({
-    message: "pong",
-  });
+app.get("/ping", async (req: express.Request, res: express.Response) => {
+  res.send({ message: "pong" });
 });
 
 app.listen(PORT, () => {
-  console.log("Server is running on port", PORT);
+  console.log(`listening on ${PORT}`);
 });
