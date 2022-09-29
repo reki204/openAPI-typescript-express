ebf4bedf118a6c2ea3fb96966ee3d08a2fa6d488
Onda Reki <rekionda0524@ezweb.ne.jp>
2022-09-29 10:29:59 +0900

    morgan追加

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import express from 'express';
+import morgan from 'morgan';
 const PORT: string | number = process.env.PORT || 8000;
 const app: express.Application = express();
+
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(express.static('public'));
 
 app.get('/hello', async (req: express.Request, res: express.Response) => {
   res.send({ message: 'hello' });
